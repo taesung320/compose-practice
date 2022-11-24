f3c1cc4c209dddc7750fbdfda25cc2774f763512
pjoon357 <pjoon357@naver.com>
2022-11-24 16:45:23 +0900

    예제 table 추가

--- backend/target/classes/schema.sql
+++ backend/target/classes/schema.sql
@@ -12,3 +12,18 @@ CREATE TABLE IF NOT EXISTS example.files (
     path varchar(200) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS example.comtnbloguser (
+    BLOG_ID char(20) NOT NULL,
+    EMPLYR_ID varchar(20) NOT NULL,
+    MNGR_AT	char(1)	NOT NULL,
+    MBER_STTUS varchar(15),
+    SBSCRB_DE datetime,
+    SECSN_DE char(2),
+    USE_AT char(1),
+    FRST_REGIST_PNTTM datetime NOT NULL,
+    FRST_REGISTER_ID varchar(20) NOT NULL,
+    LAST_UPDT_PNTTM datetime,
+    LAST_UPDUSR_ID varchar(20),
+    PRIMARY KEY (BLOG_ID, EMPLYR_ID)
+);
